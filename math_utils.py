14af941145898a503762a038b60e4fe1b25a15b6
Tanayabhoi <115806525+Tanayabhoi@users.noreply.github.com>
2025-08-03 15:40:34 +0530

    Create math_utils.py

---
+++ math_utils.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Adds two numbers and returns the result
+
+    Parameters:
+    a (int): First number
+    b (int): Second number
+
+    Returns:
+    int: Sum of a and b
+    """
+    return a + b
